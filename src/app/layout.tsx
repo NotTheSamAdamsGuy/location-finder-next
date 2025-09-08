86f3a8269c15ffde86552cfdbc261781e955ee8d
Jim Koch <jimkoch52@gmail.com>
2025-09-07 20:27:03 -0700

    removed unnecessary comment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import Header from "./components/Header";
 import '@fortawesome/fontawesome-svg-core/styles.css';
 // Prevent fontawesome from adding its CSS since we did it manually above:
 import { config } from '@fortawesome/fontawesome-svg-core';
-config.autoAddCss = false; /* eslint-disable import/first */
+config.autoAddCss = false;
 
 export const metadata: Metadata = {
   title: "Create Next App",
