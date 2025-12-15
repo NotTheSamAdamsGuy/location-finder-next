ecb79d88c2f9fce5e396f744617f3deb84d24e41
Jim Koch <jimkoch52@gmail.com>
2025-12-15 11:32:20 -0800

    updated admin error page

--- src/app/admin/error.tsx
+++ src/app/admin/error.tsx
@@ -1,5 +1,25 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 export default function ErrorPage() {
-  return <div>Error!</div>;
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.refresh();
+  };
+
+  return (
+    <div className="absolute top-1/3 w-full">
+      <div className="flex flex-col items-center">
+        <h1 className="text-6xl font-semibold mb-8">Oops!</h1>
+        <p className="mb-8 px-8 text-center text-lg">
+          An unexpected error has occurred. Please reload the page.
+        </p>
+        <button className="btn btn-primary flex" onClick={handleClick}>
+          Reload
+        </button>
+      </div>
+    </div>
+  );
 }
