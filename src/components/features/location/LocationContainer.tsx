5f5b38f4a36c9b1ddb9b195d9ff1bee4263370e8
Jim Koch <96154041+NotTheSamAdamsGuy@users.noreply.github.com>
2026-02-04 10:50:52 -0800

    image carousel on location details page (#19)

--- src/components/features/location/LocationContainer.tsx
+++ src/components/features/location/LocationContainer.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import { getImageUrl } from "@/lib/utils/imageUtils";
 import { LocationFeature } from "@notthesamadamsguy/location-finder-types";
-import Image from "next/image";
 import MobileNavBar from "./MobileNavBar";
 import { useRef } from "react";
 import { useIntersection } from "@/hooks/useIntersection";
 import TagsContainer from "./TagsContainer";
+import LocationImageCarousel from "./LocationImageCarousel";
 
 interface Props {
   location: LocationFeature;
@@ -16,24 +15,6 @@ export default function LocationContainer({ location }: Props) {
   const { name, address, city, state, postalCode, description } =
     location.properties;
   
-    const locationImages: React.ReactNode[] = location?.properties?.images
-    ? location.properties.images.map((imageData) =>   {
-        const imageUrl = getImageUrl(imageData);
-        const imageAlt = imageData.description ?? "location image";
-        return (
-          <Image
-            key={imageAlt}
-            src={imageUrl}
-            alt={imageAlt}
-            width={768}
-            height={412}
-            className="w-full h-72 object-cover object-center"
-            priority
-          />
-        );
-      })
-    : [];
-
   const locationImagesRef = useRef(null);
   const locationImagesAreVisible = useIntersection(locationImagesRef, 0.25);
 
@@ -42,7 +23,9 @@ export default function LocationContainer({ location }: Props) {
       <MobileNavBar
         className={`w-full h-0 ${locationImagesAreVisible ? "" : "bg-base-100 h-18 shadow-sm"}`}
       />
-      <div ref={locationImagesRef} className={`${locationImagesAreVisible ? "mt-0" : "-mt-18"}`}>{locationImages[0]}</div>
+      <div ref={locationImagesRef} className={`${locationImagesAreVisible ? "mt-0" : "-mt-18"}`}>
+        <LocationImageCarousel images={location.properties.images || []} />
+      </div>
       <div className="p-3">
         <h1 className="text-3xl font-bold">{name}</h1>
         <div>
