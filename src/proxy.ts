3862e74cb67a6a5e3ed2e78630fc43340dea8e7a
Jim Koch <jimkoch52@gmail.com>
2026-03-08 19:09:41 -0700

    removed home page from list of protected pages

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { decrypt } from "@/lib/session";
 
-const protectedRoutes = ["/", "/admin", "/admin/locations", "/admin/locations/add"];
+const protectedRoutes = ["/admin", "/admin/locations", "/admin/locations/add"];
 const publicRoutes = ["/login"];
 
 export default async function proxy(req: NextRequest) {
